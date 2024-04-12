57c71af2f2f85d7d73e787f99b07829fb4a519b0
Jonah Kunzler <jonahk12@gmail.com>
2024-04-12 14:48:20 -0600

    new changes made

--- src/test/java/app/view/MainPresenterTest.java
+++ src/test/java/app/view/MainPresenterTest.java
@@ -6,45 +6,130 @@ import app.model.requestresponse.Response;
 import app.service.Service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MainPresenterTest {
 
-    /*TODO: Write tests to assure that the Presenter and View classes interact as expected.  If the services throw an error,
-    *  the view.displayErrorMessage() function should be called inside the presenter class.  You will need to use
-    *  mock objects to effectively test the presenter.
-    */
-    private MainPresenter classUnderTest;
+  private MainPresenter classUnderTest;
+  private I_MainView mockView;
+  private Service mockTitleService;
+  private Service mockAuthorService;
+  private Service mockTopicService;
 
-    @BeforeEach
-    void setUp(){
-        I_MainView view = Mockito.mock(I_MainView.class);
-        Service service = Mockito.mock(Service.class);
+  @BeforeEach
+  void setUp() {
+    mockView = Mockito.mock(I_MainView.class);
+    mockTitleService = Mockito.mock(Service.class);
+    mockAuthorService = Mockito.mock(Service.class);
+    mockTopicService = Mockito.mock(Service.class);
 
-        try {
-            Mockito.when(service.run(new Request("Exception"))).thenThrow(IOException.class);
-        } catch (IOException ignored) {
+    classUnderTest = new MainPresenter(mockView, mockTitleService, mockAuthorService, mockTopicService);
 
-        }
-
-        classUnderTest = new MainPresenter(view, service, service, service);
-
+    try {
+      Mockito.when(mockTitleService.run(new Request("Exception"))).thenThrow(new IOException("Network error"));
+      Mockito.when(mockAuthorService.run(new Request("Exception"))).thenThrow(new IOException("Network error"));
+      Mockito.when(mockTopicService.run(new Request("Exception"))).thenThrow(new IOException("Network error"));
+    } catch (IOException e) {
+      //
     }
+  }
 
-    @Test
-    void shouldPrintErrorMessage_whenReceiveInvalidResponse() throws IOException {
-        classUnderTest.titleSearch("Exception");
-        Mockito.verify(classUnderTest.getTitleService(), Mockito.times(1)).run(Mockito.any());
+  @Test
+  void titleSearch_ShouldDisplayResults_WhenInputIsValid() throws IOException {
+    String validInput = "Valid Title";
+    Book[] expectedBooks = new Book[]{ new Book(validInput, "Author Name", new String[]{"Topic"}) };
+    Response mockResponse = new Response(expectedBooks);
+    Mockito.when(mockTitleService.run(new Request(validInput))).thenReturn(mockResponse);
+    classUnderTest.titleSearch(validInput);
+    Mockito.verify(mockTitleService).run(new Request(validInput));
+    Mockito.verify(mockView).displayTitleSearchResults(expectedBooks);
+  }
 
-        Mockito.verify(classUnderTest.getView(), Mockito.times(1)).displayErrorMessage(Mockito.anyString());
-    }
+  @Test
+  void titleSearch_ShouldDisplayErrorMessage_WhenInputIsInvalid() {
+    String invalidInput = "";
+    classUnderTest.titleSearch(invalidInput);
+    Mockito.verify(mockView).displayErrorMessage("Invalid input.  Please enter a valid title.");
+  }
+
+
+  @Test
+  void titleSearch_ShouldDisplayErrorMessage_OnException() throws IOException {
+    String input = "Exception";
+    classUnderTest.titleSearch(input);
+    Mockito.verify(mockTitleService).run(new Request(input));
+    Mockito.verify(mockView).displayErrorMessage("An exception occurred: Network error");
+  }
+
+  @Test
+  void authorSearch_ShouldDisplayResults_WhenInputIsValid() throws IOException {
+    String validInput = "Valid Author";
+    Book[] expectedBooks = new Book[]{ new Book("Title", validInput, new String[]{"Topic"}) };
+    Response mockResponse = new Response(expectedBooks);
+    Mockito.when(mockAuthorService.run(new Request(validInput))).thenReturn(mockResponse);
+    classUnderTest.authorSearch(validInput);
+    Mockito.verify(mockAuthorService).run(new Request(validInput));
+    Mockito.verify(mockView).displayAuthorSearchResults(expectedBooks);
+  }
+
+  @Test
+  void authorSearch_ShouldDisplayErrorMessage_WhenInputIsInvalid() {
+    String invalidInput = "";
+
+    classUnderTest.authorSearch(invalidInput);
+
+    Mockito.verify(mockView).displayErrorMessage("Invalid input.  Please enter a valid author name.");
+  }
+
+
+  @Test
+  void authorSearch_ShouldDisplayErrorMessage_OnException() throws IOException {
+    String input = "Exception";
+
+    classUnderTest.authorSearch(input);
+
+    Mockito.verify(mockAuthorService).run(new Request(input));
+    Mockito.verify(mockView).displayErrorMessage("An exception occurred: Network error");
+  }
+
+
+
+  @Test
+  void topicSearch_ShouldDisplayResults_WhenInputIsValid() throws IOException {
+    String validInput = "Valid Topic";
+    Book[] expectedBooks = new Book[]{
+            new Book("Book Title 1", "Author Name 1", new String[]{validInput}),
+            new Book("Book Title 2", "Author Name 2", new String[]{validInput})
+    };
+    Response mockResponse = new Response(expectedBooks);
+    Mockito.when(mockTopicService.run(new Request(validInput))).thenReturn(mockResponse);
+
+    classUnderTest.topicSearch(validInput);
+
+    Mockito.verify(mockTopicService).run(new Request(validInput));
+    Mockito.verify(mockView).displayTopicSearchResults(expectedBooks);
+  }
+
+
+
+  @Test
+  void topicSearch_ShouldDisplayErrorMessage_OnException() throws IOException {
+    String input = "Exception";
+
+    classUnderTest.topicSearch(input);
+
+    Mockito.verify(mockTopicService).run(new Request(input));
+    Mockito.verify(mockView).displayErrorMessage("An exception occurred: Network error");
+  }
+  @Test
+  void topicSearch_ShouldDisplayErrorMessage_WhenInputIsInvalid() {
+    String invalidInput = "";
+
+    classUnderTest.topicSearch(invalidInput);
+
+    Mockito.verify(mockView).displayErrorMessage("Invalid input.  Please enter a valid topic.");
+  }
 
 
 }
